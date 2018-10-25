41b1b6fb252a2d67e4b032994855bd38bf387224
Yi JIANG <jiang.yi@siphty.com>
2018-10-26 01:57:57 +1100

    Update: Preparing Unit Test for the ServerManager and the model. TODO: Finished it.

--- VideoCatalogueTests/VideoCatalogueTests.swift
+++ VideoCatalogueTests/VideoCatalogueTests.swift
@@ -24,11 +24,61 @@ class VideoCatalogueTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testVideoCatalogueApiServiceWithCorrectApi() {
+        let apiClient = ApiClient()
         self.measure {
-            // Put the code you want to measure the time of here.
+            apiClient.networkRequest(.videoCatalogue
+                , completionHandler: { (data, error) in
+                    guard let data = data else {
+                        print("Error: \(error)")
+                        return
+                    }
+                    
+            })
         }
     }
+    
+    func testVideoCatalogueModelBasedOnMockData() {
+        
+    }
 
+}
+
+
+class MockApiClient: ApiClient {
+    
+    enum JsonFileName: String {
+        case vcResponse = "VideoCatalogueAPIResponse"
+        case vcResponse_empty = "VideoCatalogueAPIResponse_empty"
+    }
+    
+    var jsonFileName = JsonFileName.vcResponse
+    
+    //Use mock response data
+    override func networkRequest(_ config: ApiConfig, completionHandler: @escaping ((Data?, RequestError?) -> Void)) {
+        guard let json = JsonFileLoader.loadJson(fileName: jsonFileName.rawValue) as? Data else {
+            completionHandler(nil, RequestError("Video Catalogue information failed."))
+            return
+        }
+        completionHandler(json, nil)
+    }
+}
+
+class JsonFileLoader {
+    
+    class func loadJson(fileName: String) -> Any? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            if let data = NSData(contentsOf: url) {
+                do {
+                    return try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions(rawValue: 0))
+                } catch {
+                    print("Error!! Unable to parse  \(fileName).json")
+                }
+            }
+            print("Error!! Unable to load  \(fileName).json")
+        } else {
+            print("invalid url")
+        }
+        return nil
+    }
 }
