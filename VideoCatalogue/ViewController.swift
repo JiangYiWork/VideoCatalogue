e70b89a3a4fdceaf97574df5f2c45dffaa4f1822
Yi JIANG <jiang.yi@siphty.com>
2018-10-25 14:06:15 +1100

    Initial commit with blank project

---
+++ VideoCatalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoCatalogue
+//
+//  Created by Yi JIANG on 24/10/18.
+//  Copyright © 2018 Siphty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
